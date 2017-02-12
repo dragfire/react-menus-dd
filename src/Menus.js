26a6b0a614ff1b2da48af1bed17984a6ff7f6af6
Devajit Asem <devajit.asem@graphiti.xyz>
2017-02-12 14:38:55 +0530

    Add Component lifecycles

--- src/Menus.js
+++ src/Menus.js
@@ -1,33 +1,78 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Menu from './Menu';
+import SubMenus from './SubMenus';
+import { WrappedComponent, TriggerComponent } from './HelperComponents';
 import './menus.css';
 
-const Menus = (props) => {
-  const {
-    list,
-    children,
-    style,
-  } = props;
-  const className = 'Menus';
-  let MenuList = (
-    <ul className={className} style={style}>
-      {children}
-    </ul>
-  );
+class Menus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
 
-  if (!list) {
-    MenuList = (
-      <div className={className} style={style}>
-        {children}
+    this.onDOMClick = this.onDOMClick.bind(this);
+    this.onClickedAway = this.onClickedAway.bind(this);
+    this.toggleMenus = this.toggleMenus.bind(this);
+  }
+
+  componentDidMount() {
+    this.element = ReactDOM.findDOMNode(this);
+    window.addEventListener('click', this.onDOMClick);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onDOMClick);
+  }
+
+  onDOMClick(event) {
+    if (this.element !== event.target && !this.element.contains(event.target)) {
+      this.onClickedAway();
+    }
+  }
+
+  onClickedAway() {
+    this.setState({ visible: false });
+  }
+
+  toggleMenus() {
+    this.setState({ visible: !this.state.visible });
+  }
+
+  render() {
+    const {
+      list,
+      children,
+      style,
+      label,
+      triggerComponent,
+    } = this.props;
+    return (
+      <div className="Menus-wrapper">
+        <WrappedComponent
+          toggleMenus={this.toggleMenus}
+          component={triggerComponent}
+          label={label}
+        />
+        <div className={`${this.state.visible ? 'show' : 'hide'}`}>
+          <SubMenus
+            list={list}
+            style={style}
+          >
+            {children}
+          </SubMenus>
+        </div>
       </div>
     );
   }
-  return MenuList;
-};
+}
 
 Menus.propTypes = {
   list: React.PropTypes.bool,
+  label: React.PropTypes.string,
   style: React.PropTypes.object,
+  triggerComponent: React.PropTypes.func,
   children: React.PropTypes.oneOfType([
     React.PropTypes.element,
     React.PropTypes.func,
@@ -38,6 +83,8 @@ Menus.propTypes = {
 Menus.defaultProps = {
   list: true,
   style: null,
+  label: null,
+  triggerComponent: TriggerComponent,
 };
 
 export default Menus;
